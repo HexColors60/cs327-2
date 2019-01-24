ba2ad6f229661e2aaf88fe6f3cb277319b46cbcf
aidans <aidans@iastate.edu>
2019-01-24 12:37:47 -0600

    Added corridor drawing and room location picking

--- sherburne_aidan.assignment-1.01/generate_dungeon.c
+++ sherburne_aidan.assignment-1.01/generate_dungeon.c
@@ -5,12 +5,12 @@
 
 #define ROOM_MIN_X 4
 #define ROOM_MIN_Y 3
-#define ROOM_MAX_X 12
-#define ROOM_MAX_Y 12
+#define ROOM_MAX_X 10
+#define ROOM_MAX_Y 8
+#define MIN_DIST 12
 #define MIN_ROOMS 6
-#define MAX_ROOMS 10
 #define NUM_ROWS 21
-#define NUM_COLS 90
+#define NUM_COLS 80
 
 char ROCK = ' ';
 char FLOOR = '.';
@@ -56,12 +56,77 @@ void draw_corridor(int x0, int y0, int x1, int y1) {
   }
 }
 
+void print_dungeon(){
+  int i,j;
+  for (j = 0; j < NUM_ROWS; j++) {
+    for (i = 0; i < NUM_COLS; i++) {
+      printf("%c",dungeon[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int get_num_rooms(int seed){
+  return (seed % MIN_ROOMS) + MIN_ROOMS;
+}
+
+int draw_room(int seed, int x, int y){
+  srand(seed);
+
+  int w = rand() % ROOM_MAX_X;
+  if(w < ROOM_MIN_X) w = w + ROOM_MIN_X;
+
+  int h = rand() % ROOM_MAX_Y;
+  if(h < ROOM_MIN_Y) h = h + ROOM_MIN_Y;
+
+}
+
+int get_dist(int x0, int y0, int x1, int y1){
+  return (int)sqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0));
+}
+
+int generate_rooms(int seed, int num_rooms){
+  int r; //rooms generated
+  int last_x = 0, last_y = 0;
+  srand(seed);
+  for(r = 0; r < num_rooms; r++){
+    
+
+    int x = 0;
+    while(x >= NUM_COLS - 1 || x < 1){
+      x = rand() % NUM_COLS;
+    }
+    int y = 0;
+    while(y >= NUM_ROWS - 1 || y < 1){
+      y = rand() % NUM_ROWS;
+    }
+    
+    //dungeon[x][y] = 'X';
+    if(last_x != 0 && last_y != 0){
+      draw_corridor(last_x, last_y, x, y);
+    }
+    //draw_room(seed, x, y);
+    last_x = x;
+    last_y = y;
+  }
+}
+
 int main(int argc, char * argv[]) {
   int seed;
+ 
+  srand(time(NULL));
   if (argc == 2)
-    srand(strtol(argv[1], NULL, 10));
+    seed = strtol(argv[1], NULL, 10);
   else
-    srand(time(NULL));
+    seed = rand();
+ 
+  int num_rooms = get_num_rooms(seed);
+  printf("seed: %d, num rooms: %d\n",seed,num_rooms);
+  init_dungeon();
+  generate_rooms(seed, num_rooms);
 
+
+  print_dungeon();
+  
   return 0;
 }
