4c57e25dc83421a397764b8c1edc90030983ee34
aidans <aidans@iastate.edu>
2019-04-05 21:41:05 -0500

    Create item.cpp

---
+++ sherburne_aidan.assignment-1.08/item.cpp
@@ -0,0 +1,15 @@
+#include "dungeon.h"
+#include "item.h"
+#include "dice.h"
+#include "utils.h"
+#include "descriptions.h"
+#include vector
+
+
+void gen_items(dungeon_t *d)
+{
+}
+
+void destroy_items(dungeon_t *d)
+{
+}
