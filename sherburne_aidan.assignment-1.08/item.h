43d4b977ebb75c50420283fa8ed56b5ee5098602
aidans <aidans@iastate.edu>
2019-04-05 21:40:13 -0500

    Create item.h

---
+++ sherburne_aidan.assignment-1.08/item.h
@@ -0,0 +1,28 @@
+#ifndef ITEM_H
+# define ITEM_H
+
+# include <string>
+
+# include "descriptions.h"
+# include "dims.h"
+
+class item {
+ private:
+  object_description &odesc;
+  std::string &name;
+  std::string &description;
+  object_type_t type;
+  uint32_t color;
+  uint32_t hit;
+  dice &damage;
+  uint32_t dodge;
+  uint32_t def;
+  uint32_t weight;
+  uint32_t speed;
+  uint32_t attr;
+  uint32_t val;
+  uint32_t art;
+  uint32_t rrty;
+  pair_t position;
+};
+#endif
